460de07f012e1c05703b7dd98a4dd6a878fc2c7d
s4b3l0 <sabelo.nkosi@rocketmail.com>
2023-02-20 20:53:24 +0200

    up to question 1.4

--- CoreService.java
+++ CoreService.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 final class BatchDate {
@@ -36,10 +39,22 @@ class Bakery {
 
     final List<String> salesProducts = new ArrayList<>();
     final HashMap<Integer, LocalDate> expDate = new HashMap<>();
-    final HashMap<BatchDate, PriorityQueue> expStock = new HashMap<>();
+    final HashMap<BatchDate, List> expStock = new HashMap<>();
+
+    final List<String> getSalesProducts = new ArrayList<>();
+
     final List<String> soldProducts = new ArrayList<>();
 
+    static int getBatchNumber(String name) {
+        if (name == null) return -1;
+        if (name.split(" ").length != 3) return -1;
+        if (!name.split(" ")[1].equalsIgnoreCase("batch")) return -1;
+        return Integer.parseInt(name.split(" ")[2]);
+    }
 
+    public HashMap<BatchDate, List> getExpStock() {
+        return expStock;
+    }
 
     /**
      * This method will list batches on standard IO
@@ -59,6 +74,7 @@ class Bakery {
         System.out.println(CoreService.ANSI_RESET);
     }
 
+
     void listInventory() {
         System.out.println(CoreService.ANSI_YELLOW);
         final String firstField = "| %-40s |";
@@ -74,13 +90,58 @@ class Bakery {
         System.out.println(CoreService.ANSI_RESET);
     }
 
-    static int getBatchNumber(String name) {
-        if (name == null) return -1;
-        if (name.split(" ").length != 3) return -1;
-        if (!name.split(" ")[1].equalsIgnoreCase("batch")) return -1;
-        return Integer.parseInt(name.split(" ")[2]);
+    void listInventory(List<String> list, LocalDate expDate) {
+        System.out.println(CoreService.ANSI_YELLOW);
+        final String firstField = "| %-40s |";
+        final String field = " %-40s |";
+        final String heading1 = String.format(firstField, "INVENTORY").replace(' ', '_');
+        final String heading2 = String.format(field, "EXPIRY DATES").replace(' ', '_');
+        System.out.println(heading1 + heading2);
+        list.forEach(prod -> {
+            final String product = String.format(firstField, prod);
+            final String expiryDate = String.format(field, expDate);
+            System.out.println(product + expiryDate);
+        });
+        System.out.println(CoreService.ANSI_RESET);
     }
 
+    public void removeExpiredBatches() {
+        final List<Integer>  batchToDelete = new ArrayList<>(); //To avoid concurrency exception
+        this.expDate.forEach((batchNo, date) -> {
+            if (date.isBefore(LocalDate.now())){
+               final BatchDate batchDate = new BatchDate(batchNo, date);
+               final List<String> expiredStock = salesProducts
+                       .stream()
+                       .filter(salesProduct -> getBatchNumber(salesProduct) == batchNo)
+                       .collect(Collectors.toList());
+               expStock.put(batchDate, expiredStock);
+               salesProducts.removeIf(salesProduct -> getBatchNumber(salesProduct) == batchNo);
+               batchToDelete.add(batchNo);
+            }
+        });
+        batchToDelete.forEach(batchNo -> expDate.remove(batchNo));
+    }
+
+    public void sellIt() {
+        final List<Integer>  batchToDelete = new ArrayList<>(); //To avoid concurrency exception
+        this.expDate.forEach((batchNo, data ) -> {
+            if (data.isAfter(LocalDate.now())) {
+                final List<String> Sold = salesProducts;
+                soldProducts.addAll( salesProducts
+                        .stream().filter(soldProduct -> getBatchNumber(soldProduct) == batchNo) //is stream framework allowed ?
+                        .map(prod -> prod.substring(0, prod.lastIndexOf(' ')))
+                        .collect(Collectors.toList()));
+                salesProducts.removeIf(salesProduct -> getBatchNumber(salesProduct) == batchNo);
+                batchToDelete.add(batchNo);
+            }
+        });
+        //sold batch removing from expDate
+        batchToDelete.forEach(batchNo -> expDate.remove(batchNo));
+    }
+
+    public void loadSheddingAdjust() {
+        expDate.forEach(((batchNo, localDate) -> expDate.put(batchNo, localDate.minusDays(1))));
+    }
 }
 
 
@@ -90,7 +151,8 @@ public class CoreService {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws NoSuchMethodException {
         Bakery bakery = new Bakery();
         bakery.salesProducts.add("Croissant batch 220");
         bakery.salesProducts.add("Croissant batch 220");
@@ -110,9 +172,7 @@ public class CoreService {
     }
 
 
-
-
-    private static void reduceSelection(char selected, Bakery bakery) {
+    private static void reduceSelection(char selected, Bakery bakery) throws NoSuchMethodException {
         String input = null;
         switch (selected) {
             case 'A':
@@ -161,8 +221,56 @@ public class CoreService {
             case 'E':
                 bakery.listBatch();
                 break;
-            case 'S':
+            case 'D':
+                //Remove expired batches from stock load them to expired data
+                while (input == null) {
+                    System.out.print(ANSI_RED + "System will start clearing of expired inventory, do you with to continue [ Y / N ]?\n:" + ANSI_RESET);
+                    final Scanner scanner = new Scanner(System.in);
+                    input = scanner.nextLine();
+                    if (input.length() == 1) {
+                        switch (input.toUpperCase().charAt(0)) {
+                            case 'Y':
+                                bakery.removeExpiredBatches();
+                                bakery.expStock.forEach((batchDate, list)  -> {
+                                    System.out.println(ANSI_YELLOW + "\nBatch Group:"  + batchDate.getBatchNumber() + "\nExpired:" + batchDate.getExpDate());
+                                    bakery.listInventory(list, batchDate.getExpDate());
+                                });
+                                break;
+                            case 'N': break;
+                            default:
+                                System.out.println(ANSI_RED + "Unknown operation selected" + ANSI_RESET);
+                                input = null;
+                        }
+                    } else {
+                        System.out.println(ANSI_RED + "Unknown operation selected" + ANSI_RESET);
+                        input = null;
+                    }
+                }
                 break;
+            case 'S':
+                while (input == null) {
+                    final String message = "Sell all unexpired inventory to continue enter Y to confirm [ Y / N ]?";
+                    try {
+                        input = generiDialogPrompt(message, bakery ,bakery.getClass().getDeclaredMethod("sellIt"));
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            case 'U':
+               while (input == null) {
+                   final String message = "Adjust expiry dates for inventory items [ Y / N ]?";
+                   try {
+                       input = generiDialogPrompt(message, bakery ,bakery.getClass().getDeclaredMethod("loadSheddingAdjust"));
+                   } catch (InvocationTargetException e) {
+                       throw new RuntimeException(e);
+                   } catch (IllegalAccessException e) {
+                       throw new RuntimeException(e);
+                   }
+               }
+               break;
             case 'X':
                 System.exit(0);
                 break;
@@ -170,6 +278,26 @@ public class CoreService {
         reduceSelection(prompt(), bakery);
     }
 
+    static String generiDialogPrompt(String message, Object object, Method method) throws InvocationTargetException, IllegalAccessException {
+            System.out.print(ANSI_RED + message + "\n:" + ANSI_RESET);
+            final Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() == 1) {
+                switch (input.toUpperCase().charAt(0)) {
+                    case 'Y':
+                        method.invoke(object);
+                        return input;
+                    case 'N':
+                        return input;
+                    default:
+                        System.out.println(ANSI_RED + "Unknown operation selected" + ANSI_RESET);
+                        return input;
+                }
+            } else {
+                System.out.println(ANSI_RED + "Unknown operation selected" + ANSI_RESET);
+                return input;
+            }
+    }
     /**
      * This method will print list available inventory on the standard io
      */
@@ -179,10 +307,10 @@ public class CoreService {
         System.out.println(ANSI_RESET);
         System.out.println("Type A to add croissant");
         System.out.println("Type D to remove croissant");
-        System.out.println("Type U update expiry dates to croissant");
+        System.out.println("Type U update expiry dates of inventory(Load Shedding)");
         System.out.println("Type L list product and expiry dates");
-        System.out.println("Type B list batch and expiry dates");
-        System.out.println("Type S list and sell by freshest");
+        System.out.println("Type E list batch and expiry dates");
+        System.out.println("Type S sell all unexpired products");
         System.out.println("Type X to exit");
         System.out.print(ANSI_GREEN + ":");
 
